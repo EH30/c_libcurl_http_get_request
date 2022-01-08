b0e903a5785dc5e2297a5999233f7d3f96b32d76
EH30 <games.eh2000@gmail.com>
2022-01-08 12:31:48 +0530

    Add files via upload

--- req.c
+++ req.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "libcurl/include/curl/curl.h"
 
+
+size_t got_data(char *buffer, size_t itemsize, size_t nitems, void* ignorethis) {
+    size_t bytes = itemsize * nitems;
+    for (int i = 0; i < bytes; i++) {
+        printf("%c", buffer[i]);
+    }
+    return bytes;
+}
+
 int main(void) {
     CURL *curl;
     CURLcode res;
@@ -11,7 +20,8 @@ int main(void) {
         return 1;
     }
 
-    curl_easy_setopt(curl, CURLOPT_URL, "www.google.com");
+    curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, got_data);
     res = curl_easy_perform(curl);
 
     if (res != CURLE_OK) {
