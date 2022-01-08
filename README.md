83b9463e451ebfb555de129d30a0be9d84ac106b
EH30 <games.eh2000@gmail.com>
2022-01-08 14:10:46 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -3,5 +3,4 @@
 Compile: gcc req.c -o req.exe -L "libcurl/lib" -lcurl   
 run: open cmd and cd to the folder where the req.exe file is located then type req.exe   
 </br>
-
 You can find the libcurl-x64.dll in libcurl/bin just copy it and paste in the folder with the req.exe   
--- req.c
+++ req.c
@@ -19,8 +19,12 @@ int main(void) {
         printf("error: (%d)\n", curl);
         return 1;
     }
-
+    
     curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0");
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    // curl_easy_setopt(curl, CURLOPT_CAINFO, "cacert.pem");
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, got_data);
     res = curl_easy_perform(curl);
 
@@ -31,7 +35,6 @@ int main(void) {
 
     long response_code;
     curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-
     printf("\nResponse Code: [%ld]\n", response_code);
 
     curl_easy_cleanup(curl);
